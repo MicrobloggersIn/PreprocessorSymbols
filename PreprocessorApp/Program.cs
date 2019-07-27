ebbf441d2c93adeaef4342b5f2b25b7989dd7f72
Vikash Chauhan <vikashchauhan51@gmail.com>
2019-07-27 20:10:36 +0530

    Sample of Preprocessor Symbols and configuration

---
+++ PreprocessorApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PreprocessorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if Microbloggers
+            Console.WriteLine("Hello World debug mode!");
+#endif
+
+#if MicrobloggersRelease
+            Console.WriteLine("Hello World release mode!");
+#endif
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# PreprocessorSymbols
+Sample of Preprocessor Symbols and configuration in c#.Here you will learn to create your own build configuration and compiler symbols.
